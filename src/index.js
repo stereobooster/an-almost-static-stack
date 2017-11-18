daaf5f02e3f22a363cc9b9f66671283bb0b17856
stereobooster <stereobooster@gmail.com>
2017-11-18 22:48:16 +0100

    Latest and greatest react-snap

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
-// import { hydrate, render } from 'react-dom';
-import { render } from 'react-dom';
+import { hydrate, render } from 'react-dom';
+// import { render } from 'react-dom';
 import App from './App'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 
 const rootElement = document.getElementById('root');
-// if (rootElement.hasChildNodes()) {
-//   hydrate(<App />, rootElement);
-// } else {
-//   render(<App />, rootElement);
-// }
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
 
-render(<App />, rootElement);
+// render(<App />, rootElement);
 
 registerServiceWorker();
