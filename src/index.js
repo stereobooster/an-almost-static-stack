a06c8946079507bebfd049d56babd9114a8ba80d
stereobooster <stereobooster@gmail.com>
2017-12-09 21:20:49 +0100

    Switch to react-snap, enable inlineCSS

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import { render } from 'react-snapshot'
+import { hydrate, render } from "react-dom";
 import App from './App'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker';
 
-const rootEl = document.getElementById('root')
-render(<App />, rootEl)
+const rootElement = document.getElementById("root");
 
-if (module.hot) {
-  module.hot.accept('./App', () => {
-    const NextApp = require('./App').default
-    render(<NextApp />, rootEl)
-  })
+if (rootElement.hasChildNodes()) {
+  // some bug prevents to do hydrate, will figure out later
+  // hydrate(<App />, rootElement);
+  render(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+
+  if (module.hot) {
+    module.hot.accept('./App', () => {
+      const NextApp = require('./App').default
+      render(<NextApp />, rootElement)
+    })
+  }
 }
 
 registerServiceWorker();
