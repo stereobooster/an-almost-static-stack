668524c46649d28c9e70580ef3db2e5d3d08e4af
stereobooster <stereobooster@gmail.com>
2017-12-10 00:04:17 +0100

    Workaround for Firebase bug

--- scripts/add-push-headers.js
+++ scripts/add-push-headers.js
@@ -6,9 +6,13 @@ const HTTP2_PUSH_MANIFEST = "../build/http2-push-manifest.json";
 const fs = require("fs");
 const path = require("path");
 
+// filter(x => x.source !== "") is workaround for
+// Error: HTTP Error: 400, hosting.headers[2].source does not meet minimum length of 1
+// error Command failed with exit code 1.
 const http2PushManifest = JSON.parse(
   fs.readFileSync(path.join(__dirname, HTTP2_PUSH_MANIFEST))
-);
+).filter(x => x.source !== "");
+
 const firebaseBasicConfig = JSON.parse(
   fs.readFileSync(path.join(__dirname, FIREBASE_BASIC_CONFIG))
 );
