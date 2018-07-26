e92ad5f7049cf23bbf99545215d9c5d14d69b3fe
stereobooster <stereobooster@gmail.com>
2018-07-26 23:53:55 +0200

    Can not reproduce error

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
+process.env.BABEL_ENV = 'development';
+process.env.NODE_ENV = 'development';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
@@ -100,9 +100,9 @@ measureFileSizesBeforeBuild(paths.appBuild)
     }
   );
 
-// Create the production build and print the deployment instructions.
+// Create the developmnet build and print the deployment instructions.
 function build(previousFileSizes) {
-  console.log('Creating an optimized production build...');
+  console.log('Creating a developmnet production build...');
 
   let compiler = webpack(config);
   return new Promise((resolve, reject) => {
